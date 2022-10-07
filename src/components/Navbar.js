5b42bb6022c5421656d90c5d80ae24d22c0427d2
PMagatteS <papamagattesady@gmail.com>
2022-10-07 14:48:16 +0000

    deleted and added dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,6 @@
-import React from "react";
-import { Toolbar, AppBar, Typography, IconButton } from "@material-ui/core";
-import { AddCircleOutlineSharp } from "@material-ui/icons";
-// import useStyles from "../style";
-import useStyles from "../style";
-
 const Navbar = () => {
   const classes = useStyles();
-  return (
-    <>
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h2" component="div" className={classes.title}>
-            Todo List
-          </Typography>
-          {/* <div className={classes.icon}></div> */}
-          <IconButton className={classes.AddIcon} size="large" color="inherit">
-            <AddCircleOutlineSharp />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+  return <></>;
 };
 
 export default Navbar;
